7148f0b42ce4cd0c8c7cc7d76902b8a6c79e07a8
wpc1996 <littlefw9812@student.faytechcc.edu>
2026-06-10 12:56:35 -0400

    Add files via upload

---
+++ p1Lab1LittlefieldWilliam.py
@@ -0,0 +1,8 @@
+#William Littlefield
+#june 10 2026
+#P1LAB1
+#Learning the basics of python
+
+First_Name = input("Enter your first name: ")
+Last_Name = input("Enter your last name: ")
+print("Hello,",First_Name,Last_Name +"!","Welcome to CTI-110!")
